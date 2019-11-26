a319d5e148fb378a9686d570f113b91e366943aa
Rizraelle <admin@rizraelle.local>
2019-11-26 14:33:54 +0300

    Right import for usdt

--- coins/eth/index.js
+++ coins/eth/index.js
@@ -1,7 +1,7 @@
 import bat from './bat.svg';
 import mkr from './mkr.svg';
 import iost from './iost.svg';
-import usdt from './iost.svg';
+import usdt from './usdt.svg';
 
 export default {
   bat,
